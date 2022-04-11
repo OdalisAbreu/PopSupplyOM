97b9b243e79323840da6e6ba963b96489bc5bb2e
Dariel Abreu <odalisdabrue@gmail.com>
2022-04-11 11:28:49 -0400

    add User only

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(UsersTableSeeder::class);
-         $this->call(CategoriesSeeder::class);
-         $this->call(ProductsTableSeeder::class);
-         $this->call(ProductImageSeeder::class);
-         $this->call(DirrectionTableSeeder::class);
+  //       $this->call(CategoriesSeeder::class);
+  //       $this->call(ProductsTableSeeder::class);
+  //       $this->call(ProductImageSeeder::class);
+  //       $this->call(DirrectionTableSeeder::class);
 
     }
 }
